2e62215dd1f7f9db79a36a94fe034b1cf62de4e5
Matthew Thomas <matthew@mwt.me>
2022-04-20 17:35:31 -0500

    do PurgeCSS in build.js

--- build.js
+++ build.js
@@ -11,13 +11,17 @@
 var fs = require('fs');
 var path = require('path');
 var marked = require('marked').marked;
+var PurgeCSS = require('purgecss').PurgeCSS;
 
 
-/* Export Markdown to JSON Array */
 const readmePath = './readme-text/';
-const jsonPath = './dist/assets/readme-text.json'
+const jsonPath = './dist/assets/readme-text.json';
+const stylePath = './dist/assets/css/style.css';
 var readmeData = {};
 
+/*=============================================================================
+ Export Markdown to JSON Array 
+=============================================================================*/
 fs.readdirSync(readmePath).forEach(file => {
     const filePath = path.join(__dirname, readmePath, file);
     const fileName = file.split('.').slice(0, -1).join('.');
@@ -32,3 +36,35 @@ fs.writeFileSync(jsonPath, JSON.stringify(readmeData), err => {
         return
     }
 });
+
+/*=============================================================================
+ PurgeCSS Section 
+=============================================================================*/
+
+/* Make an array of html from markdown that purgeCSS needs */
+htmlArray = Object.values(readmeData).map(html => {
+    return {
+        raw: html,
+        extension: 'html'
+    }
+});
+/* add the index */
+htmlArray.push('./index.html')
+
+/* set config options */
+const purgeCSSResult = new PurgeCSS().purge({
+    content: htmlArray,
+    css: [stylePath],
+    safelist: ["sb-sidenav-toggled", "active", "mb-4"]
+});
+
+/* write the output to the dist css file */
+(async function () {
+    var x = await purgeCSSResult;
+    fs.writeFileSync(stylePath, x[0].css, err => {
+        if (err) {
+            console.error(err)
+            return
+        }
+    });
+})();
