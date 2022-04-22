a55d3396e2e7364582433dda300171c108bd9b0e
Matthew W. Thomas <matthew@mwt.me>
2022-04-22 12:31:58 -0500

    make async more sensible though it's not really faster

--- build.js
+++ build.js
@@ -45,34 +45,63 @@ fs.mkdirSync(assetPath, { recursive: true });
  Make header.html and footer.html Files (plus blank assets/index.html)
 =============================================================================*/
 
-const indexPath = path.normalize('./index.html');
-const headerPath = path.join(distPath, "header.html");
-const footerPath = path.join(distPath, "footer.html");
+/* nothing depends on this */
+(async () => {
+    const indexPath = path.normalize('./index.html');
+    const headerPath = path.join(distPath, "header.html");
+    const footerPath = path.join(distPath, "footer.html");
 
-/* write blank html file to assets dir */
-fs.writeFile(path.join(assetPath, "index.html"), "", (err) => { if (err) throw err; });
+    /* write blank html file to assets dir */
+    fs.writeFile(path.join(assetPath, "index.html"), "", (err) => { if (err) throw err; });
 
-/* read the index.html file to split it into header and footer */
-fs.readFile(indexPath, 'utf8', (err, data) => {
-    /* use regex with named groups to extract the header and footer from index.html */
-    const headerRegex = "<!--Begin header\.html-->\n(?<header>[^]+?)<!--End header\.html-->";
-    const middleRegex = "(?<middle>[^]+?)";
-    const footerRegex = "<!--Begin footer\.html-->\n(?<footer>[^]+?)<!--End footer\.html-->";
-    const re = new RegExp(headerRegex + middleRegex + footerRegex);
-    const matchedText = data.match(re);
+    /* read the index.html file to split it into header and footer */
+    fs.readFile(indexPath, 'utf8', (err, data) => {
+        /* use regex with named groups to extract the header and footer from index.html */
+        const headerRegex = "<!--Begin header\.html-->\n(?<header>[^]+?)<!--End header\.html-->";
+        const middleRegex = "(?<middle>[^]+?)";
+        const footerRegex = "<!--Begin footer\.html-->\n(?<footer>[^]+?)<!--End footer\.html-->";
+        const re = new RegExp(headerRegex + middleRegex + footerRegex);
+        const matchedText = data.match(re);
 
-    /* write header.html (async) */
-    fs.writeFile(headerPath, matchedText.groups.header, (err) => { if (err) throw err; });
+        /* write header.html (async) */
+        fs.writeFile(headerPath, matchedText.groups.header, (err) => { if (err) throw err; });
 
-    /* write footer.html (async) */
-    fs.writeFile(footerPath, matchedText.groups.footer, (err) => { if (err) throw err; });
-});
+        /* write footer.html (async) */
+        fs.writeFile(footerPath, matchedText.groups.footer, (err) => { if (err) throw err; });
+    });
+})();
+
+
+/*=============================================================================
+     Export Markdown to JSON Array
+=============================================================================*/
+
+/* we use this output later */
+markdownArrayHTML = (async () => {
+    const readmePath = path.normalize('./readme-text/');
+    const jsonPath = path.join(assetPath, 'readme-text.json');
+    var readmeData = {};
+
+    fs.readdirSync(readmePath).forEach(file => {
+        const filePath = path.join(readmePath, file);
+        const fileName = file.split('.').slice(0, -1).join('.');
+        const mdString = fs.readFileSync(filePath, 'utf8');
+        const htmlString = md.render(mdString);
+        readmeData[`/${fileName}/`] = `${htmlString}<hr>`;
+    });
+
+    /* no need to block with this */
+    fs.writeFile(jsonPath, JSON.stringify(readmeData), (err) => { if (err) throw err; });
+
+    return readmeData;
+})();
 
 
 /*=============================================================================
  UglifyJS Section
 =============================================================================*/
 
+/* nothing depends on this */
 minify({
     compressor: uglifyjs,
     input: './assets/js/script.js',
@@ -85,67 +114,53 @@ minify({
  CSSO Section
 =============================================================================*/
 
-minify({
+/* we make a promise because we need it in the next section */
+var cssoPromise = minify({
     compressor: csso,
     input: './assets/css/*.css',
     output: './dist/assets/style.css',
-    callback: afterCSS
 });
 
 
 /*=============================================================================
-     Export Markdown to JSON Array and PurgeCSS
+ PurgeCSS Section
 =============================================================================*/
 
-async function afterCSS(err, min) {
+/* make the raw html array that purgecss needs */
+make_htmlArray = (async () => {
+    readmeData = await markdownArrayHTML;
+    /* Make an array of html from markdown that purgeCSS needs */
+    htmlArray = Object.values(readmeData).map(html => {
+        return {
+            raw: html,
+            extension: 'html'
+        };
+    });
+    /* add the index */
+    htmlArray.push('./index.html');
 
-    /* Export Markdown to JSON Array
-       ============================= */
-    const readmePath = path.normalize('./readme-text/');
-    const jsonPath = path.join(assetPath, 'readme-text.json');
-    var readmeData = {};
+    return htmlArray;
+})();
 
-    fs.readdir(readmePath, (err, files) => {
-        if (err) throw err;
-        files.forEach(file => {
-            const filePath = path.join(readmePath, file);
-            const fileName = file.split('.').slice(0, -1).join('.');
-            const mdString = fs.readFileSync(filePath, 'utf8');
-            const htmlString = md.render(mdString);
-            readmeData[`/${fileName}/`] = `${htmlString}<hr>`;
-        });
-        fs.writeFile(jsonPath, JSON.stringify(readmeData), (err) => { if (err) throw err; });
-        purgecssActions(readmeData, min);
+/* this depends on both the htmlArray and the csso operation */
+Promise.all([cssoPromise, make_htmlArray]).then(out => {
+    const minCSS = out[0];
+    const htmlArray = out[1];
+
+    /* set config options */
+    const purgeCSSResult = new PurgeCSS().purge({
+        output: './dist/assets/style.css',
+        content: htmlArray,
+        css: [{ raw: minCSS, extension: 'css' }],
+        safelist: ["sb-sidenav-toggled", "active"]
     });
 
-    /* PurgeCSS Section
-       ================ */
-    /* This depends on the HTML made from markdown. So, it's run in the callback */
-    async function purgecssActions(readmeData, min) {
-        const stylePath = './dist/assets/style.css';
-
-        /* Make an array of html from markdown that purgeCSS needs */
-        htmlArray = Object.values(readmeData).map(html => {
-            return {
-                raw: html,
-                extension: 'html'
-            };
-        });
-        /* add the index */
-        htmlArray.push('./index.html');
-
-        /* set config options */
-        const purgeCSSResult = new PurgeCSS().purge({
-            content: htmlArray,
-            css: [{ raw: min, extension: 'css' }],
-            safelist: ["sb-sidenav-toggled", "active"]
-        });
-
-        /* write the output to the dist css file */
-        var x = await purgeCSSResult;
+    /* write the output to the dist css file */
+    const stylePath = './dist/assets/style.css';
+    purgeCSSResult.then(x => {
         fs.writeFile(stylePath, x[0].css, err => { if (err) throw err; });
-    }
-}
+    });
+});
 
 
 /*=============================================================================
